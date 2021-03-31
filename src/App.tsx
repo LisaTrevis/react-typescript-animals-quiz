4fcf5592936d285f092334bc9da12fb08806a6b0
Lisa Trevis <l_trevis@hotmail.com>
2021-03-30 20:11:17 -0700

    Started scaffolding, replaced images

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+// components
+import QuestionCard from './components/QuestionCard'
 
-function App() {
-  return <div className='App'>Quiz</div>
+const App = () => {
+  const [loading, setLoading] = useState(false)
+  const [questions, setQuestions] = useState([])
+  const [number, setNumber] = useState(0)
+  const [userAnswers, setUserAnswers] = useState([])
+  const [score, setScore] = useState(0)
+  const [gameOver, setGameOver] = useState(true)
+
+  const startTrivia = async () => {}
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {}
+
+  const nextQuestion = () => {}
+
+  return (
+    <div className='App'>
+      <h1>React Quiz</h1>
+      <button className='start' onClick={startTrivia}>
+        Start Quiz
+      </button>
+      <p className='score'>Score:</p>
+      <p>Loading Questions...</p>
+      <QuestionCard questionNumber />
+      <button className='next' onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  )
 }
 
 export default App
